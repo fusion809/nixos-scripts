5fc81dd870af7e2408bf61d7801b1c0433f111c1
fusion809 <fusion809@users.noreply.github.com>
2017-06-03 11:25:10 +1000

    Adding auto-install for keychain

--- Shell/programs/02-install-apps.sh
+++ Shell/programs/02-install-apps.sh
@@ -2,6 +2,10 @@ if ! `which pastebinit > /dev/null 2>&1`; then
      sudo nix-env -i pastebinit
 fi
 
+if ! `which keychain > /dev/null 2>&1`; then
+     sudo nix-env -i keychain
+fi
+
 if ! `which hub > /dev/null 2>&1`; then
      sudo nix-env -i hub
 fi
