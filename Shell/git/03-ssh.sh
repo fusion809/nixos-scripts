21985664be36ddd1d5aaeb572e6ac446f15bfbcb
fusion809 <fusion809@users.noreply.github.com>
2017-06-03 11:13:48 +1000

    Fixing some issues related to NixOS

--- Shell/git/03-ssh.sh
+++ Shell/git/03-ssh.sh
@@ -65,16 +65,16 @@ fi
 function start_agent {
   echo "Initializing new SSH agent..."
   # spawn ssh-agent
-  /usr/bin/ssh-agent | /bin/sed 's/^echo/#echo/' > "${SSH_ENV}"
+  ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
   echo succeeded
   chmod 600 "${SSH_ENV}"
   . "${SSH_ENV}" > /dev/null
-  /usr/bin/ssh-add
+  ssh-add
 }
 
 if [[ -f "${SSH_ENV}" ]]; then
   . "${SSH_ENV}" > /dev/null
-  /bin/ps -ef | /bin/grep ${SSH_AGENT_PID} | /bin/grep ssh-agent > /dev/null || {
+  ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent > /dev/null || {
     start_agent;
   }
 else
