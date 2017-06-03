1029d537a5e19c3b48c10efb10233d6e2605166a
fusion809 <fusion809@users.noreply.github.com>
2017-06-03 11:15:57 +1000

    Installing apps script adding ! to fix repeated re-installs

--- Shell/programs/02-install-apps.sh
+++ Shell/programs/02-install-apps.sh
@@ -1,7 +1,7 @@
-if `which pastebinit > /dev/null 2>&1`; then
+if ! `which pastebinit > /dev/null 2>&1`; then
      sudo nix-env -i pastebinit
 fi
 
-if `which hub > /dev/null 2>&1`; then
+if ! `which hub > /dev/null 2>&1`; then
      sudo nix-env -i hub
 fi
