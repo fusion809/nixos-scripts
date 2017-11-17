b2d7c41b8953910e44ed4f635ed6f71248c6dcec
fusion809 <brentonhorne77@gmail.com>
2017-11-18 07:15:03 +1000

    Adding to PATH variable for root

--- root/Shell/01-variables.sh
+++ root/Shell/01-variables.sh
@@ -2,4 +2,4 @@ export CHROOT=$HOME/chroot
 # Alternative kernel det
 # eselect kernel list | grep "\*" | cut -d ' ' -f 6 | sed 's/linux\-//g'
 export KERN=$(ls -ld /usr/src/linux | cut -d ' ' -f 11 | sed 's/linux\-//g')
-export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:/run/current-system/sw/bin:/run/current-system/sw/sbin:/usr/local/bin:$HOME/.nix-profile/bin:$HOME/.nix-profile/sbin
