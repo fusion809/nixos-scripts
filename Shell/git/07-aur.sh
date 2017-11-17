fddf8c0880ea73ca8aa93427f9d83f23d8a4e719
fusion809 <brentonhorne77@gmail.com>
2017-11-18 07:16:41 +1000

    Fixing variables file for root

--- Shell/git/07-aur.sh
+++ Shell/git/07-aur.sh
@@ -1,5 +1,5 @@
-function gitaur {
-     git clone ssh://aur@aur.archlinux.org/$1.git
-}
+#function gitaur {
+#     git clone ssh://aur@aur.archlinux.org/$1.git
+#}
 
-eval `keychain -q --eval aur`
+#eval `keychain -q --eval aur`
--- root/Shell/01-variables.sh
+++ root/Shell/01-variables.sh
@@ -1,5 +1,1 @@
-export CHROOT=$HOME/chroot
-# Alternative kernel det
-# eselect kernel list | grep "\*" | cut -d ' ' -f 6 | sed 's/linux\-//g'
-export KERN=$(ls -ld /usr/src/linux | cut -d ' ' -f 11 | sed 's/linux\-//g')
 export PATH=$PATH:/run/current-system/sw/bin:/run/current-system/sw/sbin:/usr/local/bin:$HOME/.nix-profile/bin:$HOME/.nix-profile/sbin
