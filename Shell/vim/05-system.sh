f28c09005dfb6dd7a3667e769dc3e8c93ba9bf70
fusion809 <brentonhorne77@gmail.com>
2017-11-18 07:58:19 +1000

    Adding vnc func for editing /etc/nixos/configuration.nix

--- Shell/vim/05-system.sh
+++ Shell/vim/05-system.sh
@@ -1,27 +1,3 @@
-function vfs {
-    sudo vim /etc/fstab
-}
-
-function vp {
-    sudo vim /etc/portage/$1
-}
-
-function vpak {
-    vp "package.accept_keywords"
-}
-
-function vpmc {
-    vp "make.conf"
-}
-
-function vpm {
-    svd /etc/portage/package.mask
-}
-
-function vpu {
-    svd /etc/portage/package.use
-}
-
-function vs {
-    sudo vim /etc/sddm.conf
+function vnc {
+    sudo vim /etc/nixos/configuration.nix
 }
