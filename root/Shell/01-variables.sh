cff9af5806a7815daf6b830f4d1fafbb397169d0
Brenton Horne <brentonhorne77@gmail.com>
2020-04-05 22:26:06 +1000

    Adding updater script for vscode

--- root/Shell/01-variables.sh
+++ root/Shell/01-variables.sh
@@ -1,2 +1,2 @@
 export PATH=$PATH:/run/current-system/sw/bin:/run/current-system/sw/sbin:$HOME/.nix-profile/bin:$HOME/.nix-profile/sbin
-export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos-unstable:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels"
+export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels"
